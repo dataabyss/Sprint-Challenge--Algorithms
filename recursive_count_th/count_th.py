6475c5cd60d10664404599d027dc21508393c45b
dataabyss <dataabyss@users.noreply.github.com>
2020-08-16 21:14:02 -0700

    finished count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,16 +6,15 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     word = list(word)
     count = 0
-    if len(word) < 2:
-        return count
-    elif ''.join(word)[0:2] == 'th':
-        count += 1
-        del word[0:2]
-        print(count)
-        count_th(word)
-    else:
-        del word[0:2]
-        count_th(word)
+    while len(word) > 1:
+        if ''.join(word)[0:2] == 'th':
+            count += 1
+            del word[0:1] # delete first letter instead of first two
+            # print(count)
+            count_th(word)
+        else:
+            del word[0:1]
+            count_th(word)
     return count
 
 print(count_th('the'))
